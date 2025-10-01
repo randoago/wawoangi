acf2a1b7c5ec8677b1b27d75b1cf1d2ef534bb51
Rando <randoago@gmail.com>
2025-10-02 00:26:10 +0800

    first full commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Load header and footer dynamically
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("partials/header.html")
+    .then(res => res.text())
+    .then(data => {
+      document.getElementById("header").innerHTML = data;
+    });
+
+  fetch("partials/footer.html")
+    .then(res => res.text())
+    .then(data => {
+      document.getElementById("footer").innerHTML = data;
+    });
+});
